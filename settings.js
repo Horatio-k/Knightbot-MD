2fde732fd7d7eefed609c2b574eb589f1ecb86e1
Horatio-k <82378364+Horatio-k@users.noreply.github.com>
2025-01-31 19:28:45 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Knight Bot', // Your name
-  ownerNumber: '22897015253', //Your number
+  ownerNumber: '+22897015253', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
